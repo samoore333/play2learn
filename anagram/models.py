680d0535bbb91ac6b0807ec02ccb49cafa97a21c
samoore333 <samoore333@gmail.com>
2022-12-08 11:42:00 -0800

    Game models setup

--- anagram/models.py
+++ anagram/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class AnagramScore(models.Model):
+    score = models.IntegerField
+    max_number = models.IntegerField
+    operation = models.CharField
+    end_time = models.DateTimeField
+
+    def __str__(self):
+        return self.score, self.max_number, self.operation, self.end_time
