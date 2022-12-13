db4812a0d57481a2b0f7062e585ab04c55072784
samoore333 <samoore333@gmail.com>
2022-12-12 16:04:38 -0800

    Setup admin docs

--- play2learn_site/urls.py
+++ play2learn_site/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('anagram/', include('anagram.urls')),
     path('contactus/', include('contactus.urls')),
