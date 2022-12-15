e7f546827c55661e0dd06ef83d2a37b23cb7cf7e
samoore333 <samoore333@gmail.com>
2022-12-14 16:03:32 -0800

    Email setup

--- play2learn_site/settings.py
+++ play2learn_site/settings.py
@@ -130,6 +130,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# BOTTOM OF settings.py
 if os.environ.get('ENVIRONMENT') != 'production':
     from .local_settings import *
 # DON'T PUT ANYTHING BELOW THIS
