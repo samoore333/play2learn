9530dd13200bee20dc5f540f17507fd08032e898
samoore333 <samoore333@gmail.com>
2022-12-14 16:46:29 -0800

    Contact Us form setup

--- contactus/urls.py
+++ contactus/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 
+from .views import ContactUsAppView, ContactUsThanksView
+
 app_name = 'contactus'
-urlpatterns = []
+urlpatterns = [
+    path('contactus-app/', ContactUsAppView.as_view(), name='app'),
+    path('contactus-app/thanks/', ContactUsThanksView.as_view(), name='thanks'),
+]
