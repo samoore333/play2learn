cf60119ecd1452ef5e11755f14744d7d7a3c402e
samoore333 <samoore333@gmail.com>
2022-12-15 16:34:10 -0800

    Contact and Review Admin setup

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Reviews
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    model = Reviews
+    list_display = ['first_name', 'last_name', 'email', 'comment']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('first_name', 'last_name', 'email', 'comment')
+
+        return ()
--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Review(models.Model):
+class Reviews(models.Model):
     first_name = models.TextField()
     last_name = models.TextField()
     email = models.EmailField()
