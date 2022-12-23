f82306f875baf7feb1442746fd184db9518b4f71
samoore333 <samoore333@gmail.com>
2022-12-22 17:16:48 -0800

    Math Facts Practice page setup

--- mathgame/urls.py
+++ mathgame/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MathgameListView
+from .views import MathgameListView, MathGameView
 
 app_name = 'mathgame'
 urlpatterns = [
     path('', MathgameListView.as_view(), name='list'),
+    path('math-facts-practice/', MathGameView.as_view(), name='math-facts-practice'),
 ]
--- mathgame/views.py
+++ mathgame/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Mathgame
@@ -11,3 +12,6 @@ class MathgameListView(LoginRequiredMixin, ListView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class MathGameView(TemplateView):
+    template_name = 'mathgame/math_facts_practice.html'
