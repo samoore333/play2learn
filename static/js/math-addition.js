dbb513b7e33b364ba8f64c8d0d43fbc550e87130
samoore333 <samoore333@gmail.com>
2023-01-30 13:57:45 -0800

    Mathgame javascript edits

--- static/js/math-addition.js
+++ static/js/math-addition.js
@@ -2,8 +2,28 @@ window.addEventListener('load', function math() {
     let number1;
     let number2;
 
-    number1 = Math.floor((Math.random() * 10) + 1);
-    number2 = Math.floor((Math.random() * 10) + 1);
+    operator = document.getElementById('operator').innerHTML;
+    max = Number(document.getElementById('max').innerHTML);
+
+    if (operator == '+') {
+        number1 = Math.floor((Math.random() * max) + 1);
+        number2 = Math.floor((Math.random() * max) + 1);
+    } else if (operator == '-') {
+        number1 = Math.floor((Math.random() * max) + 1);
+        number2 = Math.floor((Math.random() * max) + 1);
+        if (number2 > number1) {
+                number2 = number1
+                number1 = number2
+        }
+    } else if (operator == '*') {
+        number1 = Math.floor((Math.random() * max) + 1);
+        number2 = Math.floor((Math.random() * max) + 1);
+    } else {
+        number2 = Math.floor((Math.random() * max) + 1);
+        numberx = Math.floor((Math.random() * max) + 1);
+        number1 = number2 * numberx
+    }
+
     document.getElementById('number1').innerHTML=number1;
     document.getElementById('number2').innerHTML=number2;
     document.getElementById('tbInput').focus();
@@ -11,8 +31,16 @@ window.addEventListener('load', function math() {
 window.addEventListener("keydown", function(e) {
     if (e.key === 'Enter' || e.keycode === 13) {
 
-    
-    let answer = number1 + number2;
+    if (operator == '+') {
+        answer = number1 + number2;
+    } else if (operator == '-') {
+        answer = number1 - number2;
+    } else if (operator == '*') {
+        answer = number1 * number2;
+    } else {
+        answer = number1 / number2;
+    }
+
     const checkAnswer = document.getElementById('tbInput');
     const value = checkAnswer.value;
     let playerScore = + this.document.getElementById('playerScore').innerHTML;
@@ -29,13 +57,28 @@ window.addEventListener("keydown", function(e) {
     document.querySelector('input[type=text]').value = "";               
     document.getElementById('number1').innerHTML = "";
     document.getElementById('number2').innerHTML = ""; 
-    number1 = Math.floor((Math.random() * 10) + 1);
-    number2 = Math.floor((Math.random() * 10) + 1);
+    if (operator == '+') {
+        number1 = Math.floor((Math.random() * max) + 1);
+        number2 = Math.floor((Math.random() * max) + 1);
+    } else if (operator == '-') {
+        number1 = Math.floor((Math.random() * max) + 1);
+        number2 = Math.floor((Math.random() * max) + 1);
+        if (number2 > number1) {
+                number2 = number1
+                number1 = number2
+        }
+    } else if (operator == '*') {
+        number1 = Math.floor((Math.random() * max) + 1);
+        number2 = Math.floor((Math.random() * max) + 1);
+    } else {
+        number2 = Math.floor((Math.random() * max) + 1);
+        numberx = Math.floor((Math.random() * max) + 1);
+        number1 = number2 * numberx
+    }
     document.getElementById('number1').innerHTML = number1; 
     document.getElementById('number2').innerHTML = number2;
     document.getElementById('tbInput').focus();
 
-    answer = number1 + number2
     }
 });
 });
