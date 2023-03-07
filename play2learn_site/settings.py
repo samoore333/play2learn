5c496b60306acdd3e62d877ccaa1e8d24b5860b1
samoore333 <samoore333@gmail.com>
2023-03-06 18:47:03 -0800

    Setting file edits

--- play2learn_site/settings.py
+++ play2learn_site/settings.py
@@ -172,8 +172,10 @@ PRIVATE_FILE_STORAGE = 'play2learn_site.storage_backends.PrivateMediaStorage'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # BOTTOM OF settings.py
 if os.environ.get('ENVIRONMENT') != 'production':
